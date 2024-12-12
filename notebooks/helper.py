cd0e98398d6bf6922ebaf927e49c54c7596ccd0b
Anbita Siregar <anbitasiregar@gmail.com>
2024-12-12 18:16:43 +0700

    refactored code to make ann model oo

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -1,11 +1,17 @@
 """
 This python file has helper methods that will help throughout the machine learning pipeline.
 """
+import numpy as np
+import seaborn as sns
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score, explained_variance_score
-import seaborn as sns
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LeakyReLU
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 """
 reset scores for model performance tests for different X_train variations
@@ -139,3 +145,103 @@ def sample_data(df, bin_column, frac=0.5):
     print(f"Reduced Count: {reduced_count}, Proportions: {reduced_proportions}\n")
 
     return reduced_df
+
+
+"""
+class to create ANN model
+activation functions: relu, sigmoid, softmax
+"""
+class ANN:
+  def __init__(self, input_size, hidden_layers, dropouts, output_size, learning_rate=0.01, leaky=False):
+    self.input_size = input_size
+    self.hidden_layers = hidden_layers
+    self.dropouts = dropouts
+    self.output_size = output_size
+    self.learning_rate = learning_rate
+
+    if leaky:
+      self.model = self.initialize_leaky_model()
+    else:
+      self.model = self.initialize_model()
+
+  def initialize_model_(self):
+    model = Sequential()
+    
+    # add hidden layers with dropouts
+    for index, units in enumerate(self.hidden_layers):
+      if index == 0:
+        # add input layer
+        model.add(Dense(units, input_dim=self.input_size, activation="relu"))
+      else:
+        # add hidden layers
+        model.add(Dense(units, activation="relu"))
+
+      # add dropout after each layer
+      model.add(Dropout(self.dropouts[index]))
+
+    # add output layer
+    model.add(Dense(self.output_size, activation="sigmoid"))
+
+    # compile the model
+    model.compile(optimizer=Adam(learning_rate=self.learning_rate),
+                  loss="categorical_crossentropy",
+                  metrics=["accuracy"])
+
+    return model
+    
+  def initialize_leaky_model(self):
+    model = Sequential()
+    
+    # add hidden layers with dropouts
+    for index, units in enumerate(self.hidden_layers):
+      if index == 0:
+        # add input layer
+        model.add(Dense(units, input_dim=self.input_size))
+      else:
+        # add hidden layer
+        model.add(Dense(units, activation="sigmoid"))
+
+      # add LeakyReLU after each layer
+      model.add(LeakyReLU(alpha=0.1))
+
+      # add dropout after each layer
+      model.add(Dropout(self.dropouts[index]))
+
+    # add output layer
+    model.add(Dense(self.output_size, activation="softmax"))
+
+    # compile the model
+    model.compile(optimizer=Adam(learning_rate=self.learning_rate),
+                  loss="categorical_crossentropy",
+                  metrics=["accuracy"])
+    
+    return model
+  
+  def model_summary(self):
+    return self.model.summary()
+  
+  def train_model(self, X_train, y_train, X_validation, y_validation, epoch, batch_size, callbacks=[EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True),
+                                                                                                    ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3)]):
+    return self.model.fit(
+      X_train, y_train,
+      validation_data=(X_validation, y_validation),
+      epochs=epoch,
+      batch_size=batch_size,
+      callbacks=callbacks
+    )
+
+  def predict_model(self, X_test, y_test):
+    # Predict on test data
+    y_pred_probs = self.model.predict(X_test)
+    y_pred_classes = np.argmax(y_pred_probs, axis=1)
+    
+    # Decode the predicted and true labels
+    y_test_classes = np.argmax(y_test.values, axis=1)
+    
+    # Confusion Matrix
+    print_confusion_matrix_details(y_test_classes, y_pred_classes)
+  
+  def evaluate_accuracy(self, X_test, y_test):
+    # Evaluate on the test set
+    _, test_accuracy = self.model.evaluate(X_test, y_test, verbose=True)
+    print(f"Test Accuracy: {test_accuracy:.2f}")
